8d3931b8c5abc9ea90f12b3de773eb29c74e5d2c
ThatsVie <viepaulalmt@gmail.com>
2023-09-24 14:48:19 -0700

    puts correction

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *  * main - Entry point
+ *  * main - Writing a string
  *   *
- *    * Return: Always 0 (Success)
+ *    * Return: 0
  *     */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
