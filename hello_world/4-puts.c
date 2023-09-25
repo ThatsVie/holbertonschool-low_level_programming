a858ebb204f45586a361e119c933fc68bd630985
ThatsVie <viepaulalmt@gmail.com>
2023-09-25 13:04:52 -0700

    appeasing betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Writing a string
- *   *
- *    * Return: 0
- *     */
+ * main - Writing a string
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
