f7c8acb2586f1d613910a31bd7c07266e5b796d4
ThatsVie <viepaulalmt@gmail.com>
2023-09-25 13:33:36 -0700

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - alpABET
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z' ; n++)
+	putchar(n);
+
+	for (n = 'A'; n <= 'Z' ; n++)
+	putchar(n);
+
+putchar('\n');
+return (0);
+}
