2734a9eddb35617737e11c21532e06292efe73de
ThatsVie <viepaulalmt@gmail.com>
2023-09-27 10:51:06 -0700

    is lower and main

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: parameter to be checked
+ * Return: 1 or 0
+ */
+int_islower(int C)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
