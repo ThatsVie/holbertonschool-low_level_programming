d0576643c0d666e6e4e2e35573c5d58a6d3ba937
ThatsVie <viepaulalmt@gmail.com>
2023-09-27 12:14:15 -0700

    syntax

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  *
  * @n: input
  *
- * Return : 0, 1, or -1
+ * Return : 1, 0, or -1
  */
 int print_sign(int n)
 {
@@ -19,7 +19,7 @@ int print_sign(int n)
 		_putchar ('0');
 		return (0);
 	}
-	else if (n < 0)
+	else
 	{
 		_putchar ('-');
 		return (-1);
