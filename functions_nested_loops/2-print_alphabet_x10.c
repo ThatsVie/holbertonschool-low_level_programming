90940bbcd1e3778d9f7003dee97fd1d61c2c47d7
ThatsVie <viepaulalmt@gmail.com>
2023-09-27 10:22:55 -0700

    10xalphabet and main update

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times, in lowercase, followed by a new line.
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(y);
+		}
+	_putchar('\n');
+	}
+}
