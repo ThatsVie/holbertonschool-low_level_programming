a11637c804683b6d725401b6c7decb6dba53db2a
ThatsVie <viepaulalmt@gmail.com>
2023-09-27 12:32:08 -0700

    correcting syntax

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,25 +3,26 @@
 /**
  * print_sign - prints the sign of a number
  *
- * @n: input for function
+ * @n: input to pass to function
  *
- * Return: 1, 0, or -1
+ * Return: returns 1, 0, or -1
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar ('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar ('-');
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		_putchar ('0');
-		return (0);
+		_putchar('0');
+		return (0);	
 	}
 }
