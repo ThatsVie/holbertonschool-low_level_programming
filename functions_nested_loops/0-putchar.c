83c07aafd04bcbbf379b2bead44a5c71994891ba
ThatsVie <viepaulalmt@gmail.com>
2023-09-27 09:33:56 -0700

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * main- prints _putchar
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+
+	char c[] = "_putchar";
+	int x;
+
+	for (x = 0; x <8; x++)
+	{
+		_putchar(c[x]);
+	}
+	_putchar('\n');
+
+	return (0)
+}
