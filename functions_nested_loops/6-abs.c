4fd7729cbf5b657d91716980431fa82b15da37e3
ThatsVie <viepaulalmt@gmail.com>
2023-09-27 13:02:22 -0700

    abs and main

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @x: computing absolute value
+ *
+ * Return: 0
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (-x);
+	}
+	else
+	{
+		return (x);
+	}
+}
