06e900ed78a584322a83e8133927949f03bdd56c
ThatsVie <viepaulalmt@gmail.com>
2023-09-27 18:03:53 -0700

    no mas blanco betty

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,16 +4,12 @@
  *
  * @c: character to be checked
  *
- * Return 1 or 0
+ * Return: 1 or 0
  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	
 		return (1);
-	
 	else
-	
 		return (0);
-	
 }
