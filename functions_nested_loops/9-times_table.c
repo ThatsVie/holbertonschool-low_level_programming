8102f7fddbb55f4fc47458f2ceb399049a41dc3a
ThatsVie <viepaulalmt@gmail.com>
2023-09-27 17:32:35 -0700

    9 times table and main

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int sum;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <=9; j++)
+		{
+			sum = i * j;
+			if (j == 0)
+			{
+			_putchar('0' + sum);
+			}
+				if (sum < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + sum);
+				}
+				else if (sum >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((sum / 10) + '0');
+					_putchar((sum % 10) + '0');
+				}
+				if (j > 9)
+				{
+					j = 0;
+				}
+		}
+			_putchar('\n');
+	}
+}
