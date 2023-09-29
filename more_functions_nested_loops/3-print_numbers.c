71ae42796916f81e0ab1cbca00551e3a5334cc5b
ThatsVie <viepaulalmt@gmail.com>
2023-09-29 13:11:28 -0700

    3 print

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,12 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	i = '0';
+
+	while (i <= '9')
 	{
-		_putchar(1 + '0');
+		_putchar(i);
+		i++;
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
