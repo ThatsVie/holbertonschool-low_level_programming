78f55aa44be707eb1cc030ae0800d5da8a05612d
ThatsVie <viepaulalmt@gmail.com>
2023-09-29 13:05:39 -0700

    3 print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main."
+/**
+ * print_numbers - prints numbers 0 to 9 then new line
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(1 + '0');
+	}
+		_putchar('\n');
+}
