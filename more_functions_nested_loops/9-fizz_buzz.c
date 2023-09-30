c59e5dea849c953bfe00b879d33033b806961859
ThatsVie <viepaulalmt@gmail.com>
2023-09-30 09:50:23 -0700

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	while (i <= 100)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d" , i);
+
+		if (i < 100)
+			putchar(' ');
+		else
+			putchar('\n');
+		i++;
+	}
+	return (0);
+}
