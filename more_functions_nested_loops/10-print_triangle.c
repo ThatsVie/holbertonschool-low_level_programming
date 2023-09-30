63dacdcf92fceed3b1c618032b53ef348de35a93
ThatsVie <viepaulalmt@gmail.com>
2023-09-30 11:06:00 -0700

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle, then new line
+ * @size: input
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int h, w;
+
+		for (h = 1; h <= size; h++)
+		{
+			for (w = h; w < size; w++)
+			{
+				_putchar(' ');
+			}
+			for (w = 1; w <= h; w++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}	
