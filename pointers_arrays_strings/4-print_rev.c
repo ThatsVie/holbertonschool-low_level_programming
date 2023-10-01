24f407ab6159a6fbbbf8fdd797c10195a88c92fd
ThatsVie <viepaulalmt@gmail.com>
2023-10-01 14:04:17 -0700

    rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**print_rev - prints string in reverse then new line
+ * @str: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[1] !=0)
+	{
+		i++;
+	}
+	i = i -1;
+
+	while (i >= 0)
+	{
+		putchar(s[i]);
+		i--;
+	}
+	putchar('\n');
+}
