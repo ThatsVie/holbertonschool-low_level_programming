3be62a5aa848ac7a4d3f956ef6320315d1b6a27f
ThatsVie <viepaulalmt@gmail.com>
2023-10-02 05:34:49 -0700

    Betty, i got you flowers

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,13 +5,13 @@
  * @dest: copy
  * Return: dest
  */
-char *_strcpy(char * dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
 
 	for (i = 0 ; src[i] != '\0' ; i++)
 	{
-		dest [i] = src[i];
+		dest[i] = src[i];
 	}
 	dest[i] = '\0';
 	return (dest);
