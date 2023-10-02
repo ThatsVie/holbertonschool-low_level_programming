7bf305a8461b1d95685b6c1806bac902b5990626
ThatsVie <viepaulalmt@gmail.com>
2023-10-02 04:45:08 -0700

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,8 +7,25 @@
 
 void puts_half(char *str)
 {
-	int i,l1, l2;
+	int length = strlen(str);
 
-
+	if (length % 2 == 0)
+	{
+		int half_length = length / 2;
+		for (int i = half_length; i < length; i ++)
+		{
+			putchar(str[i]);
+		}
+	}
+		else
+		{
+			int half_length = (length - 1) / 2;
+			for (int i = half_length + 1; i < length; i++)
+			{
+				putchar(str[i]);
+			}
+		}
+		putchar('\n');
+	}
 
 	
