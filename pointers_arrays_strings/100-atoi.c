aa3b6047405bbe37729b707a5e5dece1616c2b60
ThatsVie <viepaulalmt@gmail.com>
2023-10-02 10:44:36 -0700

    going back to this one for now

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,31 +10,27 @@ int _atoi(char *s)
 {
 	int result = 0;
 	int sign = 1;
+	int digit;
 
-	while (*s == ' ' || *s == '\t' || *s == '\n')
-		s++;
-
-	if (* s == '-')
+	while (*s == ' ' || *s == '\t' || *s == '\n' || *s == '\r' || *s == '+' || *s == '-')
 	{
-		sign = -1;
-		s++;
+		if (*s == '-')
+		{
+			sign = -sign;
+		}
+		s++,
 	}
 
-	while (*s >= '0' && *s <='9')
+	while (*s >= '0' && *s <= '9')
 	{
-		if (result > (INT_MAX / 10) || (result == INT_MAX / 10 && (*s - '0') > (INT_MAX % 10)))
+		digit = *s - '0';
+
+		if (result > (INT_MAX - digit) / 10)
 		{
-			if (sign == 1)
-			{
-				return INT_MAX;
-			}
-			else
-			{
-				return INT_MIN;
-			}
+			return (sign == 1) ? INT_MAX : INT_MIN;
 		}
 
-		result = result * 10 + (*s - '0');
+		result = result * 10 + digit;
 		s++;
 	}
 
