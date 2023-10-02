f4a107b32a21656311964770f5b28f620b18548d
ThatsVie <viepaulalmt@gmail.com>
2023-10-02 10:28:03 -0700

    correcting int min

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,21 +12,31 @@ int _atoi(char *s)
 	int sign = 1;
 
 	while (*s == ' ' || *s == '\t' || *s == '\n')
-	{
 		s++;
-	}
 
-	if (*s == '-')
+	if (* s == '-')
 	{
 		sign = -1;
 		s++;
 	}
 
-	while (*s >= '0' && *s <= '9')
+	while (*s >= '0' && *s <='9')
 	{
+		if (result > (INT_MAX / 10) || (result == INT_MAX / 10 && (*s - '0') > (INT_MAX % 10)))
+		{
+			if (sign == 1)
+			{
+				return INT_MAX;
+			}
+			else
+			{
+				return INT_MIN;
+			}
+		}
+
 		result = result * 10 + (*s - '0');
 		s++;
 	}
 
-	return sign * result;
+	return result * sign;
 }
