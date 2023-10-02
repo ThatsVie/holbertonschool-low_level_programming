16d4c7a607821a71345b83093715087d97540579
ThatsVie <viepaulalmt@gmail.com>
2023-10-02 05:48:07 -0700

    vamos aver si funcciona

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,2 +1,35 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - function recreation
+ * @s: input
+ * Return: 0
+ */
 
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+
+	while (*s == ' ' || *s == '\t' || *s == '\n' || *s == '\r')
+	{
+		s++;
+	}
+
+	if (*s == '-')
+	{	sign = -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		result = result * 10 + (*s - '0');
+		s++;
+	}
+
+	return result * sign;
+}
