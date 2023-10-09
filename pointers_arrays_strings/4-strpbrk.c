e0402c2ac49b3f3552e4bb9d8cd50df1eb272eba
ThatsVie <viepaulalmt@gmail.com>
2023-10-09 13:52:37 -0700

    no more white space for my dear betty

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
  * the bytes in the string accept
  * @s: string being searched
  * @accept: bytes searched for
- * Return:  a pointer to the byte in s that matches one of the
+ * Return: a pointer to the byte in s that matches one of the
  * bytes in accept, or NULL if no such byte is found
  */
 
