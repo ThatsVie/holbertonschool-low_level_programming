457735c6b9038f2449dcaf32c297222dd0427750
ThatsVie <viepaulalmt@gmail.com>
2023-10-12 09:36:45 -0700

    strlen

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		str++;
+		return (_strlen_recursion(s) + 1);
+	}
+}
