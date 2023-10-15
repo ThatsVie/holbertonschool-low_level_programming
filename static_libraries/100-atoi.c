3d395f4d5e2b30d66ba02b665bab2106a54d5225
ThatsVie <viepaulalmt@gmail.com>
2023-10-15 13:14:02 -0700

    100 atoi

---
+++ static_libraries/100-atoi.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - converts string to integer
+ * @s: input string
+ * Return: integer value converted from string
+ */
+
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+
+	while (*s)
+	{
+		if (*s == ' ')
+		{
+			s++;
+		}
+		else if (*s == '-')
+		{
+			sign = -sign;
+			s++;
+		}
+		else if (*s == '+')
+		{
+			s++;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			break;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		result = result * 10 + (*s - '0');
+		s++;
+	}
+	return (result * sign);
+}
