a48844e3d1f2135d402cd14bda2adc75065a2d41
ThatsVie <viepaulalmt@gmail.com>
2023-10-15 12:11:21 -0700

    is upper memset and strcat

--- static_libraries/0-memset.c
+++ static_libraries/0-memset.c
@@ -1,2 +1,22 @@
 #include "main.h"
+/**
+ * _memset - fills memory with constant byte
+ * @s: points to memory area
+ * @b: constant byte
+ * @n: number of bytes
+ * Return: pointer to memory area s
+ */
+
 char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+
+	return (ptr);
+}
