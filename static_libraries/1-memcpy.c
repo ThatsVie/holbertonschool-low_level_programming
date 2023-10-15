2e799d7503b2056cd288a288768fcaeb44d95986
ThatsVie <viepaulalmt@gmail.com>
2023-10-15 12:39:39 -0700

    memcpy

---
+++ static_libraries/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memcpy - copies bytes from memory area src to memory area dest
+ * @dest: pointer to destination memory area
+ * @src: pointer to source memory area being copied from
+ * @n: number of bytes to copy
+ * Return: pointer to dest
+ */
+
+char *memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ptr = dest;
+
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	return (ptr);
+}
