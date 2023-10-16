185a0db224a8f354685a4b13b7047d37a0fda0c8
ThatsVie <viepaulalmt@gmail.com>
2023-10-16 12:23:49 -0700

    1-args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number of command line arguments
+ * @argc: count of command line arguments
+ * @argv: array of strings containing command line arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
