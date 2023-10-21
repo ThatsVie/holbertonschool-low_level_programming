16ff0470c00f3d9b315b9136b72f31d0666c2348
ThatsVie <Viepaulalmt@gmail.com>
2023-10-21 14:22:45 -0700

    Betty changes

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		len1 = 0;
-	else len1 = strlen(s1);
+	else 
+		len1 = strlen(s1);
 
 	if (s2 == NULL)
 		len2 = 0;
@@ -29,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 	
-	concatenated = (char*)malloc(len1 + n + 1);
+	concatenated = (char *)malloc(len1 + n + 1);
 
 	if (concatenated == NULL)
 	{
