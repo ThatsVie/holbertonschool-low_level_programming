a8f2e7724e4489527e13c471a0ff4d69487a613e
ThatsVie <Viepaulalmt@gmail.com>
2023-10-22 12:13:19 -0700

    reworking

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,14 +15,15 @@ int *array_range(int min, int max)
 	int *arr;
 	int size, i;
 
+	size = ((max + 1) - min);
+
 	if (min > max)
 	{
 		return (NULL);
 	}
 
-	size = max - min + 1;
-
-	arr = (int *)malloc(size * sizeof(int));
+	
+	arr = malloc(sizeof(int) * size);
 
 	if (arr == NULL)
 	{
