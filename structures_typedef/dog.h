e5a8324a78d2922161dcc85d44382f6898b74e81
ThatsVie <Viepaulalmt@gmail.com>
2023-10-27 14:59:08 -0700

    New type dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type
+ * @name: pointer to  a string, dog's name
+ * @age: float, age of dog
+ * @owner: pointer to string, owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
