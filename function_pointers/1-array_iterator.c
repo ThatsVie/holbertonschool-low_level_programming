582f89c53f82398252e5c6a8d2404fbff8c2d91d
ThatsVie <Viepaulalmt@gmail.com>
2023-10-28 14:39:09 -0700

    Betty, my love for you is exemplified in the actions I've just taken for you

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,7 +4,8 @@
  * array_iterator - applies a given fuction to each element of an integer array
  * @array: pointer to integer array
  * @size: number of elements in array
- * @action: function pointer that specifies the action to be applied to each element
+ * @action: function pointer that specifies the action to be applied to
+ * each element
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
@@ -14,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (action == NULL || array == NULL)
 		return;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
