e1e0594a27bf1af201377ddb765e87646e503ae3
ThatsVie <Viepaulalmt@gmail.com>
2023-10-28 14:37:04 -0700

    Write a function that executes a function given as a parameter on each element of an array.

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - applies a given fuction to each element of an integer array
+ * @array: pointer to integer array
+ * @size: number of elements in array
+ * @action: function pointer that specifies the action to be applied to each element
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action == NULL || array == NULL)
+		return;
+
+	for(i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
