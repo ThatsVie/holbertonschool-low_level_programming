033e1f76049954851568f1bafdc2d72350161f5c
ThatsVie <Viepaulalmt@gmail.com>
2023-10-30 10:18:34 -0700

    Write a function that prints anything.

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -63,7 +63,7 @@ void print_all(const char * const format, ...)
 	char *s1 = "";
 	char *s2 = ", ";
 
-	op_t ops[] = {
+	type_t ops[] = {
 		{'c', print_char},
 		{'i', print_int},
 		{'f', print_float},
