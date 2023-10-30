cd8348c4e6a1cc035c571212740b1858f22fe2ae
ThatsVie <Viepaulalmt@gmail.com>
2023-10-30 11:27:50 -0700

    redo, different logic

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,91 +4,49 @@
 #include <stdarg.h>
 
 /**
- * print_char = prints a character
- * @args: va_list containing the character to print
- */
-
-void print_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-
-/**
- * print_int - prints an integer
- * @args: va_list containing integer to print
- */
-
-void print_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-
-/**
- * print_float - prints a float
- * @args: va_list containing the float to print
- */
-
-void print_float(va_list args)
-{
-	printf("%f", va_arg(args, double));
-}
-/**
- * print_string - prints a string
- * @args: va_list containing the string to print
- */
-
-void print_string(va_list args)
-{
-	char *s = va_arg(args, char *);
-
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", s);
-}
-
-/**
  * print_all - prints anything
- * @format: list of types of arguments to be printed
- * @...: variadic arguments to be printed
- * Return: void
+ * @format: list of arguments to print
  */
 
-void print_all(const char * const format, ...)
+void print_all (const char * const format, ...)
 {
-	va_list list;
-	int a, b;
-	char *s1 = "";
-	char *s2 = ", ";
+	va_list args;
+	char *sep = "";
+	int i = 0;
 
-	type_t ops[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
-		{'\0', NULL}
-	};
+	va_start (args, format);
 
-	va_start(list, format);
-	a = 0;
-	while (format != NULL && format[a] != '\0')
+	while (format[i])
 	{
-		b = 0;
-		while (ops[b].c != '\0')
+		if (format[i] == 'c')
 		{
-			if (ops[b].c == format[a])
-			{
-				printf("%s", s1);
-				ops[b].f(list);
-				s1 = s1;
-			}
-			b++;
+			printf("%s%c", sep, va_arg(args, int));
 		}
-		a++;
+		else if (format[i] == 'i')
+		{
+			printf("%s%d", sep, va_arg(args, int));
+		}
+		else if (format[i] == 'f')
+		{
+			printf("%s%f", sep va_args, double));
+		}
+		else if (format[i] == 's')
+		{
+			char *s = va_arg(args, char *);
+			if (s == NULL)
+			{
+				printf("%s(nil)", sep);
+			}
+			else
+			{
+				printf("%s%s", sep, s);
+			}
+		}
+
+		sep = ",";
+		i++;
 	}
+
 	printf("\n");
-	va_end(list);
+	va_end(args);
 }
-	
