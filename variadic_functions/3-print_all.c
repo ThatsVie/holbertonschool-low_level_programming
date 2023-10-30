77be4c1bcb440588f1174ded344955e11c9797e8
ThatsVie <Viepaulalmt@gmail.com>
2023-10-30 12:37:44 -0700

    redo,new way

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,5 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
 
 /**
@@ -10,43 +9,41 @@
 
 void print_all (const char * const format, ...)
 {
-	va_list args;
-	char *sep = "";
-	int i = 0;
+	va_list list;
+	int a = 0, int b = 0;
+	char *sep =", ";
+	char *str;
 
-	va_start (args, format);
+	va_start(list, format);
 
-	while (format[i])
+	while (format && format[b])
+		b++;
+	while (format && format[a])
 	{
-		if (format[i] == 'c')
+		if (a == (b -1))
 		{
-			printf("%s%c", sep, va_arg(args, int));
+			sep = "";
 		}
-		else if (format[i] == 'i')
+		switch (format[a])
 		{
-			printf("%s%d", sep, va_arg(args, int));
+			case 'c':
+				printf("%c%s, va_arg(list, int), sep);
+				break;
+			case 'i':
+				printf("%d%s", va_arg(list, int), sep);
+				break;
+			case 'f':
+				printf("%f%s", va_arg(list, double), sep);
+				break;
+			case 's':
+				str = va_arg(list), char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", str, sep);
+				break;
 		}
-		else if (format[i] == 'f')
-		{
-			printf("%s%f", sep, va_arg(args, double));
-		}
-		else if (format[i] == 's')
-		{
-			char *s = va_arg(args, char *);
-			if (s == NULL)
-			{
-				printf("%s(nil)", sep);
-			}
-			else
-			{
-				printf("%s%s", sep, s);
-			}
-		}
-
-		sep = ",";
-		i++;
+		a++;
 	}
-
-	printf("\n");
-	va_end(args);
+		printf("\n");
+		va_end(list);
 }
