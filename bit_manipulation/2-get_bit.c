540190bfcdbea5aec9b6a8fd75293adfa01b5ec6
ThatsVie <Viepaulalmt@gmail.com>
2023-11-12 15:45:47 -0800

    removed trailing whitespace and added space around >=

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -3,12 +3,12 @@
  * get_bit - returns the value of a bit at a given index
  * @n: bit
  * @index: index of the bit to retrieve, starting at 0
- * Return: value of the bit at the given index, 
+ * Return: value of the bit at the given index,
  * -1 if error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >=(sizeof(unsigned long int) * 8))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	if ((n & (1 << index)) == 0)
