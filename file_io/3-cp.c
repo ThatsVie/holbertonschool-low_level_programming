9687b513ebcebb2bc069ec22e31d3587bcb9b0a2
ThatsVie <Viepaulalmt@gmail.com>
2023-11-13 17:15:19 -0800

    corrected leftover dest to file to

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 	}
 	if (close(file_from) == -1 || close (file_to) == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't close fd %d\n",dest_fd);
+		dprintf(STDERR_FILENO,"Error: Can't close fd %d\n",file_to);
 		exit (100);
 	}
 
