075f4c132abceb923daf1cffafeb3524de6af29e
ThatsVie <Viepaulalmt@gmail.com>
2023-11-13 17:27:13 -0800

    removed struct . 11 /16 will have to suffice

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,7 +14,6 @@ int main(int argc, char *argv[])
 	int file_from, file_to;
 	ssize_t bytes_read, bytes_written;
 	char buffer[1024];
-	struct stat source_stat;
 
 	if (argc != 3)
 	{
@@ -27,7 +26,7 @@ int main(int argc, char *argv[])
 		exit (98);
 	}
 
-	if ((file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, source_stat.st_mode)) == -1)
+	if ((file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1)
 	{
 		close(file_from);
 		dprintf(STDERR_FILENO,"Error: Can't write to file %s\n", argv[2]);
