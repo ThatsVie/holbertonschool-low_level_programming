5597429a553126d0d1f571ab048a83b86f576760
ThatsVie <Viepaulalmt@gmail.com>
2023-11-17 10:26:50 -0800

    adding space before (

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -39,7 +39,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
-	if(current->next)
+	if (current->next)
 	{
 		temp = current->next;
 		temp->prev = new;
