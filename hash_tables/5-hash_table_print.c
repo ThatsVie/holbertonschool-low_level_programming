958a5bfca4784145461b94f0154dfd38008e6ddb
ThatsVie <Viepaulalmt@gmail.com>
2023-11-19 12:53:46 -0800

    added space after : in first printf

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -23,7 +23,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (first == 1)
 			{
-				printf("'%s':'%s'", node->key, node->value);
+				printf("'%s': '%s'", node->key, node->value);
 				first = 0;
 			}
 			else
