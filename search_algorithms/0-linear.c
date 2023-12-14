bb96123b1b8d5b080a62a50e0a0e141fc1988616
ThatsVie <Viepaulalmt@gmail.com>
2023-12-14 11:43:55 -0800

    spacing adjustment

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -9,22 +9,21 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t index;  /* Index of the current element being checked */
-
+	
 	/* Check for NULL array */
 	if (array == NULL)
-		 return (-1);
-
+		return (-1);
+	
 	/* Iterate through the array */
 	for (index = 0; index < size; index++)
 	{
 		/* Print the value being checked at the current index */
-		 printf("Value checked array[%lu] = [%d]\n", index, array[index]);
-
-		 /* Check if the current element is the desired value */
-		 if (array[index] == value)
-			  return (index);
+		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
+		/* Check if the current element is the desired value */
+		if (array[index] == value)
+			return (index);
 	}
 
-	 /* If the value is not found, return -1 */
+	/* If the value is not found, return -1 */
 	return (-1);
 }
